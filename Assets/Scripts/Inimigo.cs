6e5c86c41e9cf2a655adca25cae0a56ab586e169
Phns2233 <nerisdesousa2020@gmail.com>
2024-08-13 15:41:37 -0300

    att

---
+++ Assets/Scripts/Inimigo.cs
@@ -0,0 +1,71 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inimigo : MonoBehaviour
+{
+    
+    public Transform player; // Referência ao transform do jogador
+    public float moveSpeed = 3f; // Velocidade de movimento do inimigo
+    public float stopDistance = 5f; // Distância a partir da qual o inimigo para de seguir o jogador
+    public SpriteRenderer spriteR;
+    public Animator anim;
+    private bool isFollowing = true; // Variável de controle do estado de seguir
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteR = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (player != null && isFollowing)
+        {
+            // Calcula a direção para onde o inimigo deve se mover
+            Vector2 moveDirection = (player.position - transform.position).normalized;
+
+            // Move o inimigo na direção calculada
+            rb.velocity = moveDirection * moveSpeed;
+
+           /* // Ajusta a rotação do inimigo para olhar na direção do movimento
+           if (moveDirection != Vector2.one)
+            {
+                float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }*/
+
+            // Verifica se o inimigo está próximo o suficiente para parar de seguir
+            if (Vector2.Distance(transform.position, player.position) <= stopDistance)
+            {
+                isFollowing = false;
+                rb.velocity = Vector2.zero; // Para o movimento do inimigo
+            }
+
+            if (player.position.x > transform.position.x )
+            {
+              anim.SetLayerWeight(0,1);
+              //spriteR.flipY = false;
+            }
+            else if(player.position.x < transform.position.x)
+            {
+                anim.SetLayerWeight(0,1);
+                //spriteR.flipY = true;
+            }
+            if(this.gameObject.CompareTag("Player"))
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+   /*     private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("batalha");
+        }
+    }*/
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,125 @@
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Player: MonoBehaviour
+{
+    private float moveH;
+    public float moveV;
+    public int velocidade;
+    public int  attack;
+    public int vidas = 3;
+    public bool comVida = true;
+    public int dano = 30;
+    public int vida = 100;
+    private Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer sprite;
+    public string nomePorta;
+    public bool entrada;
+
+    public global::System.Single MoveH { get => moveH; set => moveH = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void FixedUpdate() {
+         MoveH = Input.GetAxis("Horizontal");
+        moveV = Input.GetAxis("Vertical");
+        transform.position += new Vector3(MoveH * velocidade * Time.deltaTime,moveV * velocidade * Time.deltaTime,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if(Input.GetKeyDown(KeyCode.W) )
+        {
+            anim.SetLayerWeight(2,1);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {   
+            anim.SetLayerWeight(1,1);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            sprite.flipX = false;
+            anim.SetLayerWeight(1,0);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            sprite.flipX = true;
+            anim.SetLayerWeight(1,0);
+        }
+        if(moveH == 0 && moveV == 0 )
+        {
+            anim.SetLayerWeight(2,0);
+            anim.SetLayerWeight(1,0);
+            anim.SetLayerWeight(0,1);
+        }
+        
+    }
+        public int VidasdoPlayer()
+        {
+            return vidas;
+        }
+        private void PerdeVida()
+        {
+            if(vida > 0)
+            {
+                vidas --;
+                vida = 100;
+            }
+            else if (vida == 0)
+            {
+                comVida = false;
+            }
+        }
+        public bool VerificaSeplayerVivo()
+        {
+            return comVida;
+        }
+        
+        private void PerdVida()
+        {
+            if(vida > 0)
+            {
+                vidas --;
+                vida = 100;
+            }
+            else if (vida == 0)
+            {
+                comVida = false;
+            }
+        }
+
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.gameObject.CompareTag("portal"))
+            {
+                nomePorta = other.gameObject.GetComponent<porta>().NomePortal();
+                entrada = other.gameObject.GetComponent<porta>().EntradaOuSaida();
+            }
+        }
+        
+        private void OnTriggerStay2D(Collider2D other) 
+        {
+            if(other.gameObject.CompareTag("portal"))
+            {
+                SceneManager.LoadScene(other.gameObject.GetComponent<porta>().NomeCena());
+            }
+        }
+            
+    }
+
+ 
+
